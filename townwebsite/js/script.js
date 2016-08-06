57b048a7685ce14b34962f50cbc8fff77f0aa225
JackMasterson <jackmasterson5@gmail.com>
2016-08-06 07:43:52 -0400

    change size and opacity using js

--- townwebsite/js/script.js
+++ townwebsite/js/script.js
@@ -1,15 +1,24 @@
 var slider = [
-	{src: 'img/stand.jpg'},
-	{src: 'img/sand.jpg'},
-	{src: 'img/boat.jpg'},
-	{src: 'img/bote.jpg'},
-	{src: 'img/pipe.jpg'}
+	{src: 'img/stand.jpg', classed: 'first'},
+	{src: 'img/sand.jpg', classed: 'second'},
+	{src: 'img/boat.jpg', classed: 'third'},
+	{src: 'img/bote.jpg', classed: 'fourth'},
+	{src: 'img/pipe.jpg', classed: 'fifth'}
 ];
 
-//for(var i=0; i<slider.length; i++){
+
 	var mid = Math.floor(slider.length/2);
-	console.log(slider[mid]);
-//	slider[mid].css('size', '250px');
-//}
+	console.log(slider[mid].classed);
+
+	$(document).ready(function(){ 
+		
+		var classy = document.getElementsByClassName(slider[mid].classed)[0];
+		console.log(classy);
+		$(classy).css('height', '250px');
+		$(classy).css('width', '250px');
+		$(classy).css('opacity', '1');
+
+	})
+	
 
 ko.applyBindings();
