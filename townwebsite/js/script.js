70ec3d2c7477f7d3174debab49bb6b421ee71331
JackMasterson <jackmasterson5@gmail.com>
2016-08-06 08:03:25 -0400

    left and right buttons working

--- townwebsite/js/script.js
+++ townwebsite/js/script.js
@@ -20,22 +20,57 @@ var slider = [
 
 	});
 
-	function shifted(){
+
+
+var shifted = {
+
+	left: function() {
 		console.log('shift!');
 		console.log(mid);
 		var classy = document.getElementsByClassName(slider[mid].classed)[0];
 		$(classy).css('height', '150px');
 		$(classy).css('width', '150px');
 		$(classy).css('opacity', '0.25');
-		mid  = mid + 1;
+		if(mid < slider.length - 1){
+			mid = mid + 1;
+		}
+		else {
+			mid = 0;
+		}
+		
 		console.log(mid);
+
 		classy = document.getElementsByClassName(slider[mid].classed)[0];
 	
 		$(classy).css('height', '250px');
 		$(classy).css('width', '250px');
 		$(classy).css('opacity', '1');
-	};
-	setTimeout(shifted, 2000);
+	},
+
+	right: function() {
+		console.log('shift!');
+		console.log(mid);
+		var classy = document.getElementsByClassName(slider[mid].classed)[0];
+		$(classy).css('height', '150px');
+		$(classy).css('width', '150px');
+		$(classy).css('opacity', '0.25');
+		//if(mid < slider.length - 1){
+			mid = mid - 1;
+		//}
+		if(mid < 0){
+			mid = slider.length - 1;
+		}
+		
+		console.log(mid);
+
+		classy = document.getElementsByClassName(slider[mid].classed)[0];
+	
+		$(classy).css('height', '250px');
+		$(classy).css('width', '250px');
+		$(classy).css('opacity', '1');
+	}
+}
+	//setTimeout(shifted, 2000);
 	
 
 ko.applyBindings();
